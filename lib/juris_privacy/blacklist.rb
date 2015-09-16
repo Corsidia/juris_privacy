8c046ff705e5735925845621347764b580e94f94
Lorenzo <lorenzo.angelini3@gmail.com>
2015-09-16 14:00:11 +0200

    refactored whitelist and blacklist

--- lib/juris_privacy/blacklist.rb
+++ lib/juris_privacy/blacklist.rb
@@ -1,28 +1,16 @@
+require_relative 'wordlist'
+
 module JurisPrivacy
   # Blacklist
-  class Blacklist
-    DEFAULT_DATA_PATH =
-      'lib/juris_privacy/blacklist_data/blacklist_words_it.data'
-
+  class Blacklist < Wordlist
     def initialize(word_source = nil)
-      if word_source.is_a? Array
-        @words = word_source
-      elsif word_source.is_a? String
-        @data_path = word_source
-      else
-        @data_path = DEFAULT_DATA_PATH
-      end
-    end
-
-    def words
-      @words ||= File
-                 .readlines(@data_path)
-                 .collect(&:chomp)
-                 .compact
+      default_data_path =
+        'lib/juris_privacy/blacklist_data/blacklist_words_it.data'
+      super(word_source, default_data_path)
     end
 
     def blacklisted?(word)
-      words.include?(word.downcase)
+      include?(word.downcase)
     end
   end
 end
